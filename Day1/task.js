f86427d8eefaed83651f070d3cdfd9183945df33
ARYAN SONI <arusoni477@gmail.com>
2024-07-13 23:02:43 +0530

    created task.js

---
+++ Day1/task.js
@@ -0,0 +1,42 @@
+//! Task 1 
+var num = 123
+console.log(num);
+
+//! Task 2
+let clg = "xyz"
+console.log(clg);
+
+//! Task 3
+const vl = true
+console.log(vl);
+
+//! Task 4
+let numb = 935;
+let prgrming = "Java"
+let value = true 
+let obj = {
+    Name: "Aryan",
+    age: 26,
+};
+
+let arr = [1,3,4,5];
+
+console.log(typeof(numb));
+console.log(typeof(prgrming));
+console.log(typeof(value));
+console.log(typeof(obj));
+console.log(typeof(arr));
+
+//! Task 5 
+let a = 5
+console.log(a);
+
+a = 6
+console.log(a);
+
+//! Task 6 
+const b = 8
+console.log(b);
+
+b = 6
+console.log(b);
