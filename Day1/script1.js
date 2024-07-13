1f3f763faeec6a4375c724a28887bd4b0f68f043
ARYAN SONI <arusoni477@gmail.com>
2024-07-13 23:09:07 +0530

    create script.js

---
+++ Day1/script1.js
@@ -0,0 +1,44 @@
+//! Number
+let num = 10;
+console.log(num)   
+console.log(typeof num)
+
+//! String
+let str = "hello";
+console.log(str)
+console.log(typeof str) 
+
+//! Boolean
+let boo = true;
+console.log(boo) 
+console.log(typeof boo)
+
+//! Null 
+let nul = null;
+console.log(nul) 
+console.log(typeof nul) 
+
+//! Undefined 
+let un = undefined;
+console.log(un)
+console.log(typeof un) 
+
+//! Symbol
+let symbol = Symbol("hello");
+console.log(symbol) 
+console.log(typeof symbol)
+
+//! BigInt
+let big_int = BigInt(10);
+console.log(big_int) 
+console.log(typeof big_int) 
+
+//! Object 
+let obj = { name: "John", age: 30 };
+console.log(obj) 
+console.log(typeof obj)
+
+//! Array
+let arr = [1, 2, 3];
+console.log(arr) 
+console.log(typeof arr)
