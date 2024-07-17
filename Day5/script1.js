7e08fe920acf0ccf487b2487cfa486ecc6b42260
ARYAN SONI <arusoni477@gmail.com>
2024-07-17 13:08:41 +0530

    updated task.js

--- Day5/script1.js
+++ Day5/script1.js
@@ -5,3 +5,4 @@ function number(num){
         console.log("Odd");
     }
 }
+
--- Day5/task.js
+++ Day5/task.js
@@ -1,7 +1,7 @@
 //! Task 1 
 
-function number(num){
-    if(num%2==0){
+function number(num) {
+    if (num % 2 == 0) {
         console.log("Even");
     } else {
         console.log("Odd");
@@ -12,10 +12,37 @@ number(2)
 
 //! Task 2
 
-function square(x){
-    return x*x
+function square(x) {
+    return x * x
 }
 console.log(square(5));
 
 //! Task 3 
 
+function maximum(num1, num2) {
+    if (num1 > num2) {
+        console.log("Num1 is greater than num2");
+    } else {
+        console.log("Num2 is gretaer than num1");
+    }
+}
+maximum(5, 9)
+
+//! Task 4 
+
+function concat1(str1) {
+    return str1
+}
+
+function concat2(str2) {
+    return str2
+}
+
+console.log(concat1 ("Hello ") +concat2("World"));
+
+//! Task 5 
+
+let sum = (x,y) => {
+    return x+y
+}
+console.log(sum(4,6))
