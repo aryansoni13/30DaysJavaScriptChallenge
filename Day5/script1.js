5dff82bd59a4763e744f7b9d2c28bde088b8c842
ARYAN SONI <arusoni477@gmail.com>
2024-07-17 13:02:21 +0530

    added few files

---
+++ Day5/script1.js
@@ -0,0 +1,7 @@
+function number(num){
+    if(num%2==0){
+        console.log("Even");
+    } else {
+        console.log("Odd");
+    }
+}
---
+++ Day5/task.js
@@ -0,0 +1,21 @@
+//! Task 1 
+
+function number(num){
+    if(num%2==0){
+        console.log("Even");
+    } else {
+        console.log("Odd");
+    }
+}
+
+number(2)
+
+//! Task 2
+
+function square(x){
+    return x*x
+}
+console.log(square(5));
+
+//! Task 3 
+
