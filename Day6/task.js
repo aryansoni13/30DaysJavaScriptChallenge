afcd7895ffd48034bd548639c6581672f005e7ae
ARYAN SONI <arusoni477@gmail.com>
2024-07-18 12:12:51 +0530

    added script2.js

--- Day6/task.js
+++ Day6/task.js
@@ -31,14 +31,18 @@
 //! Task 7
 
 const numbers = [1, 2, 3, 4, 5];
-
 const doubledNumbers = numbers.map(number => number * 2);
 console.log(doubledNumbers);  //* Output: [2,4,6,8,10]
 
 //! Task 8
+
 const evenNumbers = numbers.filter(number => number % 2 === 0);
 console.log(evenNumbers); //* Output: [2,4]
 
 //! Task 9
+
 const sumOfNumbers = numbers.reduce((accumulator, currentValue) => accumulator + currentValue, 0);
 console.log(sumOfNumbers); //* Output: 15
+
+//! Task 10 
+
