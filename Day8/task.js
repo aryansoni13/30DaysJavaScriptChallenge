3b94fc1e62d997896a2dee035ef456316ab03e36
ARYAN SONI <arusoni477@gmail.com>
2024-07-20 13:11:39 +0530

    added task.js

---
+++ Day8/task.js
@@ -0,0 +1,78 @@
+//! Task 1
+
+let personName = "Aryan"
+let age = 21
+
+console.log(`Hey, I'm ${personName} my age is ${age}`);
+
+//! Task 2 
+
+let str = `lorem 
+ipsum`;
+
+console.log(str);
+
+//! Task 3 
+
+let arr = [1,2,3,4,5,6]
+let [first, second] = arr
+console.log(first);
+console.log(second);
+
+//!Task 4 
+
+let obj = {
+    Title: "xtz",
+    Author: "Afw"
+}
+let {Title, Author} = obj
+console.log(Title);
+console.log(Author);
+
+//! Task 5 
+
+let originalArray = [1, 2, 3];
+let newArray = [...originalArray, 4, 5, 6];
+console.log(newArray);
+
+//! Task 6 
+
+function sum(...numbers) {
+    return numbers.reduce((total, num) => total + num, 0);
+}
+console.log(sum(1, 2, 3));
+console.log(sum(4, 5, 6, 7));
+
+//! Task 7 
+
+function func(num1,num2 = 1){
+    return num1*num2
+}
+console.log(func(6,5));
+console.log(func(2));
+
+//! Task 8 
+
+let name = "Aryan";
+let age1 = 22;
+
+let person = {
+    name,
+    age,
+    greet() {
+        console.log(`Hello, my name is ${this.name} and I am ${this.age1} years old.`);
+    }
+};
+console.log(person);
+person.greet();
+
+//! Task 9 
+
+let propName1 = "firstName";
+let propName2 = "lastName";
+
+let personInfo = {
+    [propName1]: "John",
+    [propName2]: "Doe"
+};
+console.log(personInfo);
